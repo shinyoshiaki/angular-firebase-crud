431dcee178cdf699d3170e17213550229f78df83
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-02-19 18:30:21 +0900

    fix typo

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Curd
+# crud
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.1.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to curd!');
+    expect(page.getTitleText()).toEqual('Welcome to crud!');
   });
 
   afterEach(async () => {
